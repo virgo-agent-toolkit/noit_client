33df9c867ca73113634c4b388f7945f0e3bba902
Nachiket Torwekar <nachiket.torwekar@rackspace.com>
2014-09-25 19:28:54 -0700

    osx1

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,11 +1,3 @@
-
-# Use 
-modprobe bridge
-
-docker -d
-
-sleep 10
-
 docker run -i -t  -P --name postgres -d nachiket/postgres
 
 docker run --name apache_borrowed -p 80:80 -p 443:443 -d eboraas/apache
